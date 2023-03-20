780400e0af18dfa04e1c7fea7b9ea1e4b04626bb
AadharshAadhithya <a.aadharsh2002@gmail.com>
2023-03-20 18:50:05 +0530

    Added database config sql

--- src/config.py
+++ src/config.py
@@ -1,6 +1,10 @@
 from dotenv import load_dotenv
 import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 load_dotenv()
 
@@ -11,5 +15,12 @@ class Config:
 
 
 
+# Database Configurations
 
+engine = create_engine('sqlite:///database.db')
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#Exports
 config = Config()
--- src/scraper.py
+++ src/scraper.py
@@ -3,7 +3,7 @@ from scholarly import scholarly
 
 class Scraper:
 
-    def __init__(self,gsq,sq=None) :
+    def __init__(self,gsq,gsq_limit=25,sq=None) :
         """
         Scrapes google and scopus for given queries and returns relevent informations
 
@@ -11,6 +11,7 @@ class Scraper:
         =======
         gsq: Google Scholar Query, expected as a list/
         sq: Scopus Query, expected as a string of scopus boolean query
+        gsq_limit: maximum number of papers to limit from google scholar search results
         
         """
         self.gsq = gsq
